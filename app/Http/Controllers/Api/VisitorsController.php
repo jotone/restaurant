6097a08ac218ee00a130df60c42d12dad0a8abc9
jotone <hereyouare1987@gmail.com>
2017-12-15 14:47:12 +0200

    order saves to DB

--- app/Http/Controllers/Api/VisitorsController.php
+++ app/Http/Controllers/Api/VisitorsController.php
@@ -2,24 +2,28 @@
 namespace App\Http\Controllers\Api;
 
 use App\MealDish;
-use App\MealMenu;
 use App\Restaurant;
+use App\VisitorOrder;
+use App\Visitors;
 
 use App\Http\Controllers\ApiController;
-use App\Visitors;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
 
 class VisitorsController extends ApiController
 {
 	public function createOrder(Request $request){
 		$data = $request->all();
 
+		//Check if User Id is empty
 		if(!isset($data['user_id'])){
 			return response(json_encode([
 				'Такого пользователя не существует'
 			]), 400);
 		}
 
+		//Decrypt ID -> get user-data by ID
+		$data['user_id'] = Crypt::decrypt($data['user_id']);
 		$user = Visitors::find($data['user_id']);
 		if(empty($user)){
 			return response(json_encode([
@@ -51,11 +55,20 @@ class VisitorsController extends ApiController
 					->first();
 				//Send SMS
 				if(!empty($restaurant)){
-					$this->sendSMStoUser($restaurant->phone, $message);
+					$result = VisitorOrder::create([
+						'visitor_id'	=> $data['user_id'],
+						'restaurant_id'	=> $restaurant_id,
+						'items'			=> json_encode($order_list),
+						'status'		=> 1
+					]);
 
-					return response(json_encode([
-						'message' => 'success'
-					]), 201);
+					if($result != false){
+						$this->sendSMStoUser($restaurant->phone, $message);
+
+						return response(json_encode([
+							'message' => 'success'
+						]), 201);
+					}
 				}
 			}
 		}else{
